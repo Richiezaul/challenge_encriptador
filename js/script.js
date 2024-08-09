e10ed917728d32c0082716101ffc8596124ddc23
Richie Zaul Martinez Reyes <richiezaul@gmail.com>
2024-08-09 19:06:52 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+var traduccion = {"a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat"};
+
+function encriptar(){
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#txtEncryptDecrypt");
+    const texto = textarea.value;
+    var area_default = document.querySelector(".content__response__noText");
+
+    console.log(area_default);
+    var area_result = document.querySelector(".content__response__text");
+    var texto_out = document.querySelector(".desncriptado__input_text");
+    if (texto != ""){
+        var out = ""
+        for(var i=0; i < texto.length; i++){
+            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
+                document.querySelector("#warning").style.color = "red";
+                document.querySelector("#warning").style.fontSize = "16px";
+                return;
+            }
+            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+            if(texto[i] == 'a'){
+                out += traduccion["a"] ;
+            }
+            else if(texto[i] == 'e'){
+                out += traduccion["e"];
+            }
+            else if(texto[i] == 'i'){
+                out += traduccion["i"]; 
+            }
+            else if(texto[i] == 'o'){
+                out += traduccion["o"]; 
+            }
+            else if(texto[i] == 'u'){
+                out += traduccion["u"]; 
+            }
+            else{
+                out += texto[i];
+            }
+        }
+
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        texto_out.innerHTML = out;
+    }
+
+    return;
+
+}
+
+function desencriptar(){
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#txtEncryptDecrypt");
+    var texto = textarea.value;
+    var area_default = document.querySelector(".content__response__noText");
+    var area_result = document.querySelector(".content__response__text");
+    var texto_out = document.querySelector(".desncriptado__input_text");
+    if (texto != ""){
+        for(var i=0; i < texto.length; i++){
+            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
+                document.querySelector("#warning").style.color = "red";
+                document.querySelector("#warning").style.fontSize = "16px";
+                return;
+            }
+            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+        }
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        texto = texto.replace(new RegExp(traduccion["a"], "g"), "a");
+        texto = texto.replace(new RegExp(traduccion["e"], "g"), "e");
+        texto = texto.replace(new RegExp(traduccion["i"], "g"), "i");
+        texto = texto.replace(new RegExp(traduccion["o"], "g"), "o");
+        texto = texto.replace(new RegExp(traduccion["u"], "g"), "u");
+        texto_out.innerHTML = texto;
+    }
+    return;
+}
+
+function clipboard(){
+    const texto_out = document.querySelector(".desncriptado__input_text");
+    navigator.clipboard.writeText(texto_out.value);
+}
+
+
+
